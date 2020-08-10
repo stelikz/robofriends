d831a3ba867203d334ee7e96ded25b8f5bd4a67c
Erwin Wijaya <stelikzz98@gmail.com>
2020-08-10 13:46:15 -0400

    text alignment

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -30,9 +30,8 @@ class App extends React.Component {
         const filteredRobots = robots.filter(robot => {
                 return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
-        return !robots.length ? 
-            <h1 className="tc">Loading</h1> : 
-        
+        return !robots.length ?
+            <h1 className="tc">Loading</h1> :         
         (
             <div className = 'tc'>
                 <h1 className='f1'>RoboFriends</h1>
